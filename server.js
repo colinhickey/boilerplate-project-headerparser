5b56a83a910b79219e60033a64e3e9b11fe6bf45
Colin <colinhickey@mac.com>
2022-04-03 18:04:01 -0400

    completed project
    returned object with required elements of header: ipaddress, language and software

--- server.js
+++ server.js
@@ -31,3 +31,13 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/whoami/", function (req, res){
+  
+  //console.log(req.headers)  
+  let ipaddress = req.headers["x-forwarded-for"];
+  let language = req.headers["accept-language"];
+  let software = req.headers["user-agent"];
+  res.json({ipaddress: ipaddress, language: language, software: software})
+  
+})
